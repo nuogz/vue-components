e39d01709b50826a0df4a9a538d7b50f5af1e616
DanoR <zheung@vip.qq.com>
2025-01-20 11:31:29 +0800

    * chore: improve develop environments * deps: bump up dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
-const plugin = require('tailwindcss/plugin');
+import plugin from 'tailwindcss/plugin.js';
 
 
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
 	content: ['./src/index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+	darkMode: ['selector', '[color-scheme="dark"]'],
 	theme: {
 		extend: {
 			boxShadow: {
